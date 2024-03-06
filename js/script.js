1e386e77c20ca59c08d8ce8a85218af8817343fa
Felipe Ferreira <fefelipe21@gmail.com>
2024-03-06 13:45:48 -0300

    slide finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import { SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 const slide = new SlideNav(".slide", ".slideWrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
-slide.addControl();
+slide.addControl('.custom-controls');
